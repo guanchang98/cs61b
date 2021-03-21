fdc8c104b749fea4218ce5568ddd67d25f096ec0
guanchang <guanchangst@163.com>
2021-03-21 22:11:07 +0800

    lab2 revised

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,7 +81,7 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        if (A == null) return null;
+        if (A == null) return B;
         IntList temp = A;
         while (temp.rest != null) {
             temp = temp.rest;
@@ -95,7 +95,7 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        if (A == null) return null;
+        if (A == null) return B;
         IntList res = new IntList(A.first, null);
         IntList temp1 = res;
         IntList temp2 = A;
