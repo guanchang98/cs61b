9220c18c4598fae3f08d3addd53a917902b81954
guanchang <guanchangst@163.com>
2021-06-02 15:33:47 +0800

    Task 5 finished

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -5,9 +5,6 @@ public class OffByN implements CharacterComparator {
     }
     @Override
     public boolean equalChars(char x, char y) {
-        if (x - 'a' < 0 || x - 'a' >= 26 || y - 'a' < 0 || y - 'a' >= 26) {
-            return false;
-        }
         int dif = x - y;
         return dif == n || dif == -n;
     }
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,9 +1,6 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        if (x - 'a' < 0 || x - 'a' >= 26 || y - 'a' < 0 || y - 'a' >= 26) {
-            return false;
-        }
         int dif = x - y;
         return dif == 1 || dif == -1;
     }
